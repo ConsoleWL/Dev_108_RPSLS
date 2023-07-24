6e8d5edbf00f1bcd033b47b4feef5f077754275e
Nikita Obergan <Obergannikita@gmail.com>
2023-07-24 15:00:51 -0400

    Add ComputerPlayer, Add Inherited from Player class, Add override method ChooseGesture

---
+++ RPSLS/ComputerPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class ComputerPlayer : Player
+    {
+        Random rnd = new Random();
+        int number;
+        public ComputerPlayer(string name) : base(name)
+        {
+            base.name = "Computer";
+        }
+        public override void ChooseGesture()
+        {
+            number = rnd.Next(1, 6);
+
+            if(number == 1)
+            {
+                chosenGesture = "rock";
+                Console.WriteLine($"{name} chooses {chosenGesture}");
+            }
+            else if(number == 2)
+            {
+                chosenGesture = "paper";
+                Console.WriteLine($"{name} chooses {chosenGesture}");
+            }
+            else if(number == 3)
+            {
+                chosenGesture = "scissors";
+                Console.WriteLine($"{name} chooses {chosenGesture}");
+            }
+            else if(number == 4)
+            {
+                chosenGesture = "lizard";
+                Console.WriteLine($"{name} chooses {chosenGesture}");
+            }
+            else if (number == 5)
+            {
+                chosenGesture = "Spock";
+                Console.WriteLine($"{name} chooses {chosenGesture}");
+            }
+        }
+    }
+}
